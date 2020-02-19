ce75987c1b3b69f68ae5910a06c22e278486b89a
Anthonius Munthi <me@itstoni.com>
2020-02-19 20:23:57 +0800

    improved openhab role

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,5 +1,5 @@
 import os
-
+import Socket
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -13,3 +13,9 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+def test_http_port(host)
+    http  = Socket('tcp://0.0.0.0:8080')
+    https = Socket('tcp://0.0.0.0:8443')
+    assert http.is_listening
+    assert https.is_listening
