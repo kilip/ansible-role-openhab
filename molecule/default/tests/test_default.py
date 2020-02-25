ff4c9992cab10fe25ff542d11b02b5c52ab4c5c5
Anthonius Munthi <me@itstoni.com>
2020-02-25 16:51:42 +0800

    added feature to customize userdata and config directory

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,3 +1,26 @@
+def test_user(host):
+    toni = host.user('toni')
+    openhab = host.user('openhab')
+
+    assert host.group('openhab').exists
+    assert host.group('toni').exists
+
+    assert toni.exists
+    assert openhab.exists
+
+
+def test_directory(host):
+    config = host.file('/srv/openhab/config')
+    userdata = host.file('/srv/openhab/userdata')
+
+    assert config.exists
+    assert config.user == 'toni'
+    assert config.group == 'openhab'
+
+    assert userdata.exists
+    assert userdata.user == 'openhab'
+    assert userdata.group == 'openhab'
+
 
 def test_hosts_file(host):
     f = host.file('/etc/hosts')
@@ -10,12 +33,14 @@ def test_hosts_file(host):
 def test_package(host):
     java = host.package('zulu-8')
     openhab = host.package('openhab2')
+
     assert java.is_installed
     assert openhab.is_installed
 
 
 def test_openhab_service(host):
     openhab = host.service('openhab2')
+    assert openhab.is_enabled
     assert openhab.is_running
 
 
